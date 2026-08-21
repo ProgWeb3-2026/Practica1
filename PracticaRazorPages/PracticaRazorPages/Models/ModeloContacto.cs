54510685aab6d4805d9295e25bcc007265bb2b57
CHRISTIAN MOBO <x.maileva@gmail.com>
2026-08-20 22:02:34 -0400

    ya esta toda esta wea weon

---
+++ PracticaRazorPages/PracticaRazorPages/Models/ModeloContacto.cs
@@ -0,0 +1,9 @@
+﻿namespace PracticaRazorPages.Models
+{
+    public class ModeloContacto
+    {
+        public string nombre {  get; set; } = string.Empty;
+        public string email { get; set; } = string.Empty;
+        public string mensaje { get; set; } = string.Empty;
+    }
+}
