5efedea1c2650e16ebfb68e89498d808839e110d
Luis Angel Polo <pololuisangel0@gmail.com>
2026-08-20 22:01:49 -0400

    POLO#

---
+++ PracticaRazorPages/PracticaRazorPages/Pages/Contacto.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PracticaRazorPages.Models;
+
+namespace PracticaRazorPages.Pages
+{
+    public class ContactoModel : PageModel
+    {
+        [BindProperty]
+        public ModeloContacto Formulario {  get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["Mensaje"] = "Formulario Enviado!";
+                return RedirectToPage();
+            }
+            return Page();
+        }
+    }
+}
