527a3a772af613ddc9117d3cbfdb00e20e1394a3
Raiza Nicole Chayana Salazar <csr4003897@est.univalle.edu>
2026-08-21 21:30:37 -0400

    Inventario, registro, agregar nuevas plantas

---
+++ PracticaRazorPages/PracticaRazorPages/Pages/Plantas/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PracticaRazorPages.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PracticaRazorPages.Pages.Plantas
+{
+    public class IndexModel : PageModel
+    {
+        public List<ModeloPlanta> Plantas { get; set; }
+
+        public void OnGet()
+        {
+            Plantas = new List<ModeloPlanta>();
+            Plantas.Add(new ModeloPlanta() {NombreComun="Tajibo", NombreCientifico= "Handroanthus", foto= "https://i.pinimg.com/736x/ea/f6/09/eaf6091a38ed6e394040f1248b303464.jpg" });
+            Plantas.Add(new ModeloPlanta() { NombreComun = "Cedro", NombreCientifico = "Cedrela odorata", foto = "" });
+
+        }
+    }
+}
