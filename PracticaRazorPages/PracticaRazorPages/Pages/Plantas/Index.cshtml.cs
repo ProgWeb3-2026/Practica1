6bfdf0000c97897d8e717ad9ea415f5e1635b191
mobo.mv <x.maileva@gmail.com>
2026-08-21 21:25:01 -0400

    Se hizo la seccion de las plantas, con mi toborochi

---
+++ PracticaRazorPages/PracticaRazorPages/Pages/Plantas/Index.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PracticaRazorPages.Models;
+
+namespace PracticaRazorPages.Pages.Plantas
+{
+    public class IndexModel : PageModel
+    {
+        private object _context;
+
+        public List<ModeloPlanta> ListaPlantas { get; set; }
+        public void OnGet()
+        {
+            ListaPlantas = new List<ModeloPlanta>();
+            ListaPlantas.Add(new ModeloPlanta() { NombreComun = "Toborochi", NombreCientifico = "Toborochi el mas gordo de todos", Foto= "https://server-bucket-2022.s3.amazonaws.com/travelapp/dd195afd822c21116c2f7da61ec792c6_1687126554710.jpeg" });
+        }
+    }
+}
